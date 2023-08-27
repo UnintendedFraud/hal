19b5460a6fd394592ff9d8a3939f26415951343a
unintendedfraud <briceunintendedfraud@fastmail.com>
2023-08-27 09:01:20 +0200

    remove log

--- main.go
+++ main.go
@@ -31,21 +31,10 @@ func main() {
 		return
 	}
 
-	fmt.Println("before .Close")
-
 	// Wait here until CTRL-C or other term signal is received.
-	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
 	<-sc
 
 	dgclient.Close()
-
-	fmt.Println("after .Close")
-	// addr := fmt.Sprintf(":%s", env.Port)
-	// fmt.Println("starting server at", addr)
-	//
-	// if err := http.ListenAndServe(addr); err != nil {
-	// 	panic(err)
-	// }
 }
