0e8fd7b734121eee582739f56bf125cef006ecdb
unintendedfraud <briceunintendedfraud@fastmail.com>
2025-05-22 09:13:06 +0200

    update model ...

--- gemini/client.go
+++ gemini/client.go
@@ -26,7 +26,7 @@ func NewClient(token string) *Client {
 	return &Client{
 		ctx:   ctx,
 		c:     gClient,
-		model: "gemini-2.5-pro-experimental-03-25",
+		model: "gemini-2.5-flash-preview-05-20",
 	}
 }
 
